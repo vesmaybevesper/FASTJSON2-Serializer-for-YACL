959c8edbe3e93ff7ae72734dc5853d6341565de0
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-29 20:42:32 -0400

    temp change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ stonecutter {
 		match("26.1", "fabric", "neoforge")
 		match("1.21.5", "fabric", "neoforge")
 		match("1.21.1", "fabric", "neoforge")
-		match("1.20.1", "fabric", "forge")
+		match("1.20.1", "fabric")
 
 		vcsVersion = "26.1-fabric"
 	}
