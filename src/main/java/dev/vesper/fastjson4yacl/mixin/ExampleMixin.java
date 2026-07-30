4901249b9282caf873665a8a12ee1f4626a6458a
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-29 21:09:32 -0400

    load bearing mixin

---
+++ src/main/java/dev/vesper/fastjson4yacl/mixin/ExampleMixin.java
@@ -0,0 +1,9 @@
+package dev.vesper.fastjson4yacl.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Minecraft.class)
+public class ExampleMixin {
+	// This mixin is load bearing lol, if it gets deleted (Lex)Forge won't build
+}
