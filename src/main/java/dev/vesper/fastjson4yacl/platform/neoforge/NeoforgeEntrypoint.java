72cd56c9bc2439e1ed67d6964d35d2fcd2c6fb3a
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-29 20:33:03 -0400

    mains and such

---
+++ src/main/java/dev/vesper/fastjson4yacl/platform/neoforge/NeoforgeEntrypoint.java
@@ -0,0 +1,15 @@
+package dev.vesper.fastjson4yacl.platform.neoforge;
+
+//? neoforge {
+
+/*import dev.vesper.fastjson4yacl.ModTemplate;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ModTemplate.MOD_ID)
+public class NeoforgeEntrypoint {
+
+	public NeoforgeEntrypoint() {
+		ModTemplate.onInitialize();
+	}
+}
+*///?}
