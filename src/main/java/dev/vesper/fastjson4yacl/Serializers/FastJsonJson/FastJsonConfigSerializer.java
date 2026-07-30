4d068adfbfe36859c1ac0fbbc4dbefd29be59496
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-29 20:44:27 -0400

    ^fix 1.20.1 no longer uses Java 21 features

--- src/main/java/dev/vesper/fastjson4yacl/Serializers/FastJsonJson/FastJsonConfigSerializer.java
+++ src/main/java/dev/vesper/fastjson4yacl/Serializers/FastJsonJson/FastJsonConfigSerializer.java
@@ -311,8 +311,7 @@ public class FastJsonConfigSerializer<T> extends ConfigSerializer<T> {
 			return LongTag.valueOf(number.longValue());
 		}
 		*///?}
-
-
+		
 		return StringTag.valueOf(value == null ? "" : value.toString());
 	}
 
