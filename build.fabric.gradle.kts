31ae447cca8b96584953fafc2ad932f9ecc901c9
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-13 13:03:48 -0400

    Chore: Update FastJson2 to 2.0.64

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 	// implementation(libs.moulberry.mixinconstraints)
 	// include(libs.moulberry.mixinconstraints)
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${prop("deps.fabric-api")}")
-	modImplementation("com.alibaba.fastjson2:fastjson2:2.0.63")
-	include("com.alibaba.fastjson2:fastjson2:2.0.63")
+	modImplementation("com.alibaba.fastjson2:fastjson2:2.0.64")
+	include("com.alibaba.fastjson2:fastjson2:2.0.64")
 	modImplementation("maven.modrinth:yacl:${property("deps.yet_another_config_lib_v3")}")
 }
--- build.neoforge.gradle.kts
+++ build.neoforge.gradle.kts
@@ -69,8 +69,8 @@ repositories {
 dependencies {
 	// implementation(libs.moulberry.mixinconstraints)
 	// jarJar(libs.moulberry.mixinconstraints)
-	implementation("com.alibaba.fastjson2:fastjson2:2.0.63")
-	jarJar("com.alibaba.fastjson2:fastjson2:2.0.63")
+	implementation("com.alibaba.fastjson2:fastjson2:2.0.64")
+	jarJar("com.alibaba.fastjson2:fastjson2:2.0.64")
 	implementation("maven.modrinth:yacl:${property("deps.yet_another_config_lib_v3")}")
 }
 
